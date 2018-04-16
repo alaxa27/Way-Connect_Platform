39ee60eea40835ee706fae8949b27b94404d8e3b
swarn <swarn.singh@viithiisys.com>
2018-04-16 15:21:16 +0530

    x3 pages design integrated

--- src/views/Campaigns/Campaigns/Campaigns.js
+++ src/views/Campaigns/Campaigns/Campaigns.js
@@ -1,104 +1,255 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import * as FontAwesome from 'react-icons/lib/fa';
+import { Container, Row, Col, Button, Input } from 'reactstrap';
+import Eye from './view.png';
+import Cart from './shopping_cart_ok.png';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
+import './campaigns.scss';
 
 class Campaigns extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+        filter: false,
+        value: 30,
+        period: 1,
+        number: 78,
+        bidWrap: false
+      }
+      this.showFilter = this.showFilter.bind(this)
+  }
+
+  showFilter(){
+    this.setState({
+      filter: !this.state.filter
+    })
+  }
+
   render() {
+    const data = [
+      {
+        rank: 1,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Delivered'   
+      },
+      {
+        rank: 2,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Bidding'   
+      },
+      {
+        rank: 3,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Bidding'   
+      },
+      {
+        rank: 4,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'  
+      },
+      {
+        rank: 5,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'    
+      },
+      {
+        rank: 6,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Bidding'   
+      },
+      {
+        rank: 7,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'      
+      },
+      {
+        rank: 8,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'      
+      },
+      {
+        rank: 9,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'      
+      },
+      {
+        rank: 10,
+        name: 'Teads.tv',
+        lastBid: '18:22',
+        view: 600,
+        bid: 6,
+        cart: 200,
+        status: 'Progress'      
+      },
+    ];
+    const showData = data.map((list, i) => 
+        <tr key={list.rank}>
+          <td><h3>#{list.rank}{i === 0 ? <FontAwesome.FaStar /> : null }</h3></td>
+          <td>
+            <label>{list.name}</label>
+            <span>last bid at {list.lastBid}</span>
+          </td>
+          <td>
+            <p>
+              <label className="pull-left">
+                <img src={Eye} alt="View" />
+                {i === 0 || i === 1 || i === 2 ? 
+                  <span style={{position: 'relative', left: -5, backgroundColor: '#fff'}}>$</span>
+                :
+                  <span style={{position: 'relative', left: -5, backgroundColor: '#cbcbcb'}}>$</span>
+                }
+              </label>
+              <label className="pull-left">{list.bid} <span className="line-through">WC</span></label>
+            </p>
+          </td>
+          <td>
+            <p>
+              <label className="pull-left"><img src={Eye} alt="View" /></label>
+              <label className="pull-left">{list.view}</label>
+            </p>
+          </td>
+          <td>
+            <p>
+              <label className="pull-left"><img src={Cart} alt="Cart" /></label>
+              <label className="pull-left">{list.cart}</label>
+            </p>
+          </td>
+        </tr>
+      )
     return (
-      <div className="animated fadeIn">
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Theme colors
-          </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-primary">Primary</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-secondary">Secondary</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-success">Success</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-danger">Danger</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-warning">Warning</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-info">Info</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-light">Light</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-dark">Dark</div>
-              </div>
-            </div>
-          </div>
+      <div className="sub-page-wrapper">
+
+        <div className="custom-breadcrumb">
+          <Row style={{width: '100%'}}>
+            <Col xs="4" md="3">
+              <label className="bidding-status-label" style={{color: '#cbcbcb'}}>
+                <FontAwesome.FaCircle />
+                Bidding
+              </label>
+            </Col>
+            <Col xs="4" md="3">
+              <label className="bidding-status-label" style={{color: '#989898'}}>
+                <FontAwesome.FaCircle />
+                In Progress
+              </label>
+            </Col>
+            <Col xs="4" md="3">
+              <label className="bidding-status-label" style={{color: '#cbcbcb'}}>
+                <FontAwesome.FaCircle />
+                Delivered
+              </label>
+            </Col>
+          </Row>
         </div>
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Grays
-          </div>
-          <div className="card-body">
-            <div className="row mb-3">
-              <div className="col-md-4">
-                <div className="p-3 bg-gray-100">100</div>
-                <div className="p-3 bg-gray-200">200</div>
-                <div className="p-3 bg-gray-300">300</div>
-                <div className="p-3 bg-gray-400">400</div>
-                <div className="p-3 bg-gray-500">500</div>
-                <div className="p-3 bg-gray-600">600</div>
-                <div className="p-3 bg-gray-700">700</div>
-                <div className="p-3 bg-gray-800">800</div>
-                <div className="p-3 bg-gray-900">900</div>
-              </div>
-            </div>
-          </div>
+
+        <div className="custom-breadcrumb">
+          <Row style={{width: '100%'}}>
+            <Col>
+              <label className="bidding-status-label" style={{color: '#989898'}} onClick={this.showFilter}>
+                <FontAwesome.FaCircle />
+                Filters fixed
+                {this.state.filter ? <FontAwesome.FaAngleDown /> : <FontAwesome.FaAngleLeft />}
+              </label>
+            </Col>
+          </Row>
         </div>
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Additional colors
-          </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-blue">Blue</div>
+
+        <Container fluid>
+          <Row>
+            <Col md="6">
+              <div className="search-list-wrap">
+                <table className="search-table">
+                  <tbody>
+                    {showData}
+                  </tbody>
+                </table>
               </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-indigo">Indigo</div>
+            </Col>
+            <Col md="6">
+              <div className="search-detail-wrap">
+                <div className="search-detail-head">
+                  <h1>Bakery</h1>
+                  <label>1950/4000 views available</label>
+                  <span>9 peoples are interested about this product</span>
+                </div>
+                <div className="bidding-panel">
+                  <div className="quote-detail">
+                    <label>Repetition</label>
+                    <InputRange maxValue={1000} minValue={0} value={this.state.value} onChange={value => this.setState({ value })} />
+                    <span className="pull-right right-slider-label">{this.state.value}</span>
+                    <div className="clearfix"></div>
+                    <br/><br/>
+                    <label>Period</label>
+                    <InputRange maxValue={12} minValue={0} value={this.state.period} onChange={period => this.setState({ period })} />
+                    <span className="pull-left right-slider-label">0</span>
+                    <span className="pull-right right-slider-label">{this.state.period} month</span>
+                    <div className="clearfix"></div>
+
+                    <Button className="get-quotations-btn">Get quotations</Button>
+                  </div>
+                </div>
               </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-purple">Purple</div>
+              <div className="bidding-area">
+                <label>Number</label>
+                <InputRange maxValue={100} minValue={0} value={this.state.number} onChange={number => this.setState({ number })} />
+                <span className="pull-left right-slider-label">0</span>
+                <span className="pull-right right-slider-label">100</span>
+                <div className="clearfix"></div>
+
+                <Row>
+                  <Col>
+                    <label className="total-cost-label">Total 250 <span className="line-through">WC</span></label>
+                    <label className="ready-status"><FontAwesome.FaCircle /> Ready for biding</label>
+                  </Col>
+                  <Col>
+                    <Button className="bid-btn">Bid</Button>
+                  </Col>
+                </Row>
+
               </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-pink">Pink</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-red">Red</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-orange">Orange</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-yellow">Yellow</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-green">Green</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-teal">Teal</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-cyan">Cyan</div>
-              </div>
-            </div>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
+        <br/>
       </div>
     )
   }
 }
-
 export default Campaigns;
--- src/views/Campaigns/CreateCampaign/CreateCampaign.js
+++ src/views/Campaigns/CreateCampaign/CreateCampaign.js
@@ -1,104 +1,184 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import * as FontAwesome from 'react-icons/lib/fa';
+import Eye from './view.png';
+import Cart from './shopping_cart_ok.png';
+import InputRange from 'react-input-range';
+import 'react-input-range/lib/css/index.css';
+import { Row, Col, Button, Input } from 'reactstrap';
 
 class CreateCampaign extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+        gender: 'male',
+        proStatus: 'salary',
+        nationality: 'indian',
+        relationaship: 'married',
+        additional: '',
+        location: 'chandigarh',
+        hobbies: 'traveling',
+        value: { min: 18, max: 24},
+      }
+      this.handleInputChange = this.handleInputChange.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.handleMultiSelect = this.handleMultiSelect.bind(this);
+  }
+
+  handleInputChange(event) {
+      this.setState({
+        gender: event.target.value
+      });
+  }
+  handleMultiSelect(selectedOption) {
+      this.setState({ selectedOption });
+  }
+  handleChange(event) {
+    const data = event.target;
+    const value = data.value;
+    const name = data.name;
+    this.setState({
+      [name]: value
+    });
+  }
   render() {
+    const tableArray = [1, 2, 3, 4, 5, 6];
+      const typologieList = tableArray.map(( list, i) => 
+        <tr key={i} className={ i === 3 ? 'full-opacity' : null }>
+          <td><label>Optician</label></td>
+          <td><label><img src={Eye} alt="View" /><span style={{position: 'relative', left: -5, backgroundColor: '#dbdbdb'}}>$</span></label></td>
+          <td><label>5 <span className="line-through">WC</span></label></td>
+          <td><label><img src={Eye} alt="View" /></label></td>
+          <td><label>3500</label></td>
+          <td><label><FontAwesome.FaUser /> &nbsp; 12</label></td>
+          <td><label><img src={Cart} alt="Cart" /> &nbsp; 500</label></td>
+        </tr>
+      )
     return (
-      <div className="animated fadeIn">
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Pheme colors
-          </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-primary">Primary</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-secondary">Secondary</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-success">Success</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-danger">Danger</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-warning">Warning</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-info">Info</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-light">Light</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-dark">Dark</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Grays
-          </div>
-          <div className="card-body">
-            <div className="row mb-3">
-              <div className="col-md-4">
-                <div className="p-3 bg-gray-100">100</div>
-                <div className="p-3 bg-gray-200">200</div>
-                <div className="p-3 bg-gray-300">300</div>
-                <div className="p-3 bg-gray-400">400</div>
-                <div className="p-3 bg-gray-500">500</div>
-                <div className="p-3 bg-gray-600">600</div>
-                <div className="p-3 bg-gray-700">700</div>
-                <div className="p-3 bg-gray-800">800</div>
-                <div className="p-3 bg-gray-900">900</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <i className="icon-drop"></i> Additional colors
-          </div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-blue">Blue</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-indigo">Indigo</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-purple">Purple</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-pink">Pink</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-red">Red</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-orange">Orange</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-yellow">Yellow</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-green">Green</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-teal">Teal</div>
-              </div>
-              <div className="col-md-4">
-                <div className="p-3 mb-3 bg-cyan">Cyan</div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="sub-page-wrapper animated fadeIn">
+        <Row>
+              <Col>
+                <h2 className="way-heading" style={{fontSize: '24px'}}><FontAwesome.FaCircle className="yellow-circle" /> #1 Typologie list</h2>
+                <Col>
+                  <Col>
+                    <table className="typology-table">
+                      <tbody>
+                        {typologieList}
+                      </tbody>
+                    </table>
+                  </Col>
+                </Col>
+              </Col>
+            </Row>
+            <br/><br/>
+            <Row>
+              <Col>
+                <h2 className="way-heading" style={{fontSize: '24px'}}><FontAwesome.FaCircle className="yellow-circle" /> #2 Research filter</h2>
+                <Col><br/>
+                  <Col md="5">
+                    <label className="input-label">Gender</label>
+                    <div className="gender-radio-buttons">
+                      <Input type="radio" id="male" name="gender" value="male" checked={this.state.gender === 'male'} onChange={this.handleInputChange} />
+                      <label htmlFor="male" className="pull-left">Male</label>
+                      <Input type="radio" id="female" name="gender" value="female" checked={this.state.gender === 'female'} onChange={this.handleInputChange} />
+                      <label htmlFor="female" className="pull-right">Female</label>
+                      <div className="clearfix"></div>
+                    </div>
+                    <div className="input-wrapper">
+                      <label className="input-label pull-left">Age</label>
+                      <label className="age-title">{this.state.value.min}-{this.state.value.max}yo</label>
+                      <div style={{paddingTop: 40}}>
+                        <InputRange maxValue={100} minValue={0} value={this.state.value} onChange={value => this.setState({ value })} />
+                      </div>
+                    </div>
+                  </Col>
+                  <div className="clearfix"></div>
+                  <Col>
+                    <div className="row" style={{margin: 0}}>
+                      <div className="col col-md-5">
+                        <div className="input-wrapper">
+                          <label className="input-label">Professional status</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="proStatus" value={this.state.proStatus} onChange={this.handleChange}>
+                              <option value="salary">Salary</option>
+                              <option value="self_employed">Self Employed</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+
+                        <div className="input-wrapper">
+                          <label className="input-label">Relationship status</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="relationaship" value={this.state.relationaship} onChange={this.handleChange}>
+                              <option value="single">Single</option>
+                              <option value="married">Married</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+
+                        <div className="input-wrapper">
+                          <label className="input-label">Nationality</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="nationality" value={this.state.nationality} onChange={this.handleChange}>
+                              <option value="indian">Indian</option>
+                              <option value="australian">Australian</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <Col md={{ size: 5, offset: 1}}>
+                        <div className="input-wrapper">
+                          <label className="input-label">Hobbies</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="hobbies" value={this.state.hobbies} onChange={this.handleChange}>
+                              <option value="technologies">Technologies</option>
+                              <option value="fishing">Fishing</option>
+                              <option value="cooking">Cooking</option>
+                              <option value="traveling">Traveling</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+
+                        <div className="input-wrapper">
+                          <label className="input-label">Location</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="location" value={this.state.location} onChange={this.handleChange}>
+                              <option value="delhi">Delhi</option>
+                              <option value="chandigarh">Chandigarh</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+
+                        <div className="input-wrapper">
+                          <label className="input-label">Additional Content</label>
+                          <div className="custom-selectbox-main">
+                            <Input type="select" className="custom-selectbox" name="additional" value={this.state.additional} onChange={this.handleChange}>
+                              <option value="additional">Additional</option>
+                            </Input>
+                            <FontAwesome.FaArrowCircleODown className="custom-selectbox-arrow" />
+                          </div>
+                        </div>
+                      </Col>
+                      <div className="clearfix"></div>
+                      <Col>
+                        <Button className="submit-button">Submit</Button>
+                      </Col>
+                    </div>
+                  </Col>
+                </Col>
+              </Col>
+            </Row>
+            <br/><br/>
       </div>
     )
   }
 }
-
 export default CreateCampaign;
+
+
+
