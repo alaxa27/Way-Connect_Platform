f49054347b36a47df0cd0be587efb587be2a5604
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-05-23 19:50:57 +0200

    Link instead of redirect function

--- src/views/Campaigns/ListCampaign/ListCampaignItem.js
+++ src/views/Campaigns/ListCampaign/ListCampaignItem.js
@@ -1,33 +1,21 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import {Redirect, withRouter} from "react-router-dom";
+import {Link} from "react-router-dom";
 import * as FontAwesome from "react-icons/lib/fa";
 
 import Eye from "./view.png";
 import Cart from "./shopping_cart_ok.png";
 
-@withRouter
-
 class ListCampaignItem extends Component {
   static propTypes = {
-    history: PropTypes.shape({
-      push: PropTypes.func
-    }),
-    item: PropTypes.shape({
-      id: PropTypes.number
-    }),
+    history: PropTypes.shape({push: PropTypes.func}),
+    item: PropTypes.shape({id: PropTypes.number}),
     status: PropTypes.string
   }
 
   constructor(props) {
     super(props);
-    this.state = {
-    };
-    this.redirectToCampaign = this.redirectToCampaign.bind(this);
-  }
-
-  redirectToCampaign() {
-    this.props.history.push(`/campaigns/${this.props.item.id}`);
+    this.state = {};
   }
 
   render() {
@@ -36,27 +24,37 @@ class ListCampaignItem extends Component {
     if (item.status === status) {
       return (<tr onClick={this.redirectToCampaign}>
         <td>
-          <label>{item.name}</label>
+          <Link to={`/campaigns/${item.id}`}>
+            <label>{item.name}</label>
+          </Link>
         </td>
         <td>
-          <label className="justify-content-center">
-            <img src={Eye} alt="View"/>
-            <span style={{
-                position: "relative",
-                left: -5,
-                fontSize: "16px"
-              }}>$</span>{` ${item.view_price}`}<span className="line-through line-through-bids">WC</span>
-          </label>
+          <Link to={`/campaigns/${item.id}`}>
+            <label className="justify-content-center">
+              <img src={Eye} alt="View"/>
+              <span style={{
+                  position: "relative",
+                  left: -5,
+                  fontSize: "16px"
+                }}>$</span>{` ${item.view_price}`}<span className="line-through line-through-bids">WC</span>
+            </label>
+          </Link>
         </td>
         <td>
-          <label className="justify-content-center">
-            <img src={Eye} alt="View"/>{` ${item.viewed}`}</label>
+          <Link to={`/campaigns/${item.id}`}>
+            <label className="justify-content-center">
+              <img src={Eye} alt="View"/>{` ${item.viewed}`}</label>
+          </Link>
         </td>
         <td>
-          <label className="justify-content-center"><FontAwesome.FaUser/>{` ${item.viewers}`}</label>
+          <Link to={`/campaigns/${item.id}`}>
+            <label className="justify-content-center"><FontAwesome.FaUser/>{` ${item.viewers}`}</label>
+          </Link>
         </td>
         <td>
-          <label className="justify-content-end"><img src={Cart} alt="Cart"/>{` ${item.bought}`}</label>
+          <Link to={`/campaigns/${item.id}`}>
+            <label className="justify-content-end"><img src={Cart} alt="Cart"/>{` ${item.bought}`}</label>
+          </Link>
         </td>
       </tr>);
     } else {
