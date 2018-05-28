533cbaddea56d7b80b00d1fb03105f46af8a07d2
volodymyrhudyma <volodymyrhudyma@gmail.com>
2018-05-28 22:45:35 +0200

    Displayed top charts on dashboard page

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -65,507 +65,6 @@ const MyMapComponent = compose(withProps({
   }
 </GoogleMap>));
 
-// Card Chart 1
-const cardChartData1 = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "transparent",
-      borderColor: "#fff",
-      data: [
-        65,
-        59,
-        84,
-        84,
-        51,
-        55,
-        40
-      ]
-    }
-  ]
-};
-
-const cardChartOpts1 = {
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          color: "transparent",
-          zeroLineColor: "transparent"
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: "transparent"
-        }
-
-      }
-    ],
-    yAxes: [
-      {
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, cardChartData1.datasets[0].data) - 5,
-          max: Math.max.apply(Math, cardChartData1.datasets[0].data) + 5
-        }
-      }
-    ]
-  },
-  elements: {
-    line: {
-      borderWidth: 1
-    },
-    point: {
-      radius: 4,
-      hitRadius: 10,
-      hoverRadius: 4
-    }
-  }
-};
-
-// Card Chart 2
-const cardChartData2 = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "transparent",
-      borderColor: "#fff",
-      data: [
-        1,
-        18,
-        9,
-        17,
-        34,
-        22,
-        11
-      ]
-    }
-  ]
-};
-
-const cardChartOpts2 = {
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  scales: {
-    xAxes: [
-      {
-        gridLines: {
-          color: "transparent",
-          zeroLineColor: "transparent"
-        },
-        ticks: {
-          fontSize: 2,
-          fontColor: "transparent"
-        }
-
-      }
-    ],
-    yAxes: [
-      {
-        display: false,
-        ticks: {
-          display: false,
-          min: Math.min.apply(Math, cardChartData2.datasets[0].data) - 5,
-          max: Math.max.apply(Math, cardChartData2.datasets[0].data) + 5
-        }
-      }
-    ]
-  },
-  elements: {
-    line: {
-      tension: 0.00001,
-      borderWidth: 1
-    },
-    point: {
-      radius: 4,
-      hitRadius: 10,
-      hoverRadius: 4
-    }
-  }
-};
-
-// Card Chart 3
-const cardChartData3 = {
-  labels: [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July"
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "transparent",
-      borderColor: "#fff",
-      data: [
-        78,
-        81,
-        80,
-        45,
-        34,
-        12,
-        40
-      ]
-    }
-  ]
-};
-
-const cardChartOpts3 = {
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false
-      }
-    ],
-    yAxes: [
-      {
-        display: false
-      }
-    ]
-  },
-  elements: {
-    line: {
-      borderWidth: 2
-    },
-    point: {
-      radius: 0,
-      hitRadius: 10,
-      hoverRadius: 4
-    }
-  }
-};
-
-// Card Chart 4
-const cardChartData4 = {
-  labels: [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-  ],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(255,255,255,.3)",
-      borderColor: "transparent",
-      data: [
-        78,
-        81,
-        80,
-        45,
-        34,
-        12,
-        40,
-        75,
-        34,
-        89,
-        32,
-        68,
-        54,
-        72,
-        18,
-        98
-      ]
-    }
-  ]
-};
-
-const cardChartOpts4 = {
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false,
-        barPercentage: 0.6
-      }
-    ],
-    yAxes: [
-      {
-        display: false
-      }
-    ]
-  }
-};
-
-// Social Box Chart
-const socialBoxData = [
-  {
-    data: [
-      65,
-      59,
-      84,
-      84,
-      51,
-      55,
-      40
-    ],
-    label: "facebook"
-  }, {
-    data: [
-      1,
-      13,
-      9,
-      17,
-      34,
-      41,
-      38
-    ],
-    label: "twitter"
-  }, {
-    data: [
-      78,
-      81,
-      80,
-      45,
-      34,
-      12,
-      40
-    ],
-    label: "linkedin"
-  }, {
-    data: [
-      35,
-      23,
-      56,
-      22,
-      97,
-      23,
-      64
-    ],
-    label: "google"
-  }
-];
-
-const makeSocialBoxData = (dataSetNo) => {
-  const dataset = socialBoxData[dataSetNo];
-  const data = {
-    labels: [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July"
-    ],
-    datasets: [
-      {
-        backgroundColor: "rgba(255,255,255,.1)",
-        borderColor: "rgba(255,255,255,.55)",
-        pointHoverBackgroundColor: "#fff",
-        borderWidth: 2,
-        data: dataset.data,
-        label: dataset.label
-      }
-    ]
-  };
-  return() => data;
-};
-
-const socialChartOpts = {
-  responsive: true,
-  maintainAspectRatio: false,
-  legend: {
-    display: false
-  },
-  scales: {
-    xAxes: [
-      {
-        display: false
-      }
-    ],
-    yAxes: [
-      {
-        display: false
-      }
-    ]
-  },
-  elements: {
-    point: {
-      radius: 0,
-      hitRadius: 10,
-      hoverRadius: 4,
-      hoverBorderWidth: 3
-    }
-  }
-};
-
-// sparkline charts
-const sparkLineChartData = [
-  {
-    data: [
-      35,
-      23,
-      56,
-      22,
-      97,
-      23,
-      64
-    ],
-    label: "New Clients"
-  }, {
-    data: [
-      65,
-      59,
-      84,
-      84,
-      51,
-      55,
-      40
-    ],
-    label: "Recurring Clients"
-  }, {
-    data: [
-      35,
-      23,
-      56,
-      22,
-      97,
-      23,
-      64
-    ],
-    label: "Pageviews"
-  }, {
-    data: [
-      65,
-      59,
-      84,
-      84,
-      51,
-      55,
-      40
-    ],
-    label: "Organic"
-  }, {
-    data: [
-      78,
-      81,
-      80,
-      45,
-      34,
-      12,
-      40
-    ],
-    label: "CTR"
-  }, {
-    data: [
-      1,
-      13,
-      9,
-      17,
-      34,
-      41,
-      38
-    ],
-    label: "Bounce Rate"
-  }
-];
-
-const makeSparkLineData = (dataSetNo, variant) => {
-  const dataset = sparkLineChartData[dataSetNo];
-  const data = {
-    labels: [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday"
-    ],
-    datasets: [
-      {
-        backgroundColor: "transparent",
-        borderColor: variant
-          ? variant
-          : "#c2cfd6",
-        data: dataset.data,
-        label: dataset.label
-      }
-    ]
-  };
-  return() => data;
-};
-
-const sparklineChartOpts = {
-  responsive: true,
-  maintainAspectRatio: true,
-  scales: {
-    xAxes: [
-      {
-        display: false
-      }
-    ],
-    yAxes: [
-      {
-        display: false
-      }
-    ]
-  },
-  elements: {
-    line: {
-      borderWidth: 2
-    },
-    point: {
-      radius: 0,
-      hitRadius: 10,
-      hoverRadius: 4,
-      hoverBorderWidth: 3
-    }
-  },
-  legend: {
-    display: false
-  }
-};
-
-// Main Chart
-
 // convert Hex to RGBA
 function convertHex(hex, opacity) {
   hex = hex.replace("#", "");
@@ -686,17 +185,19 @@ const mainChartOpts = {
     }
   }
 };
-/////////// REDUX
+
 const mapStateToProps = state => ({
+  stats: state.dashboard.stats,
 });
 
 const mapDispatchToProps = dispatch => ({
   fetchDashboardData: payload => dispatch(actions.fetchDashboardData()),
 });
-/////////
+
 export class Dashboard extends Component {
   static propTypes = {
-    fetchDashboardData: PropTypes.func.isRequired
+    fetchDashboardData: PropTypes.func.isRequired,
+    stats: PropTypes.obj
   }
   constructor(props) {
     super(props);
@@ -734,6 +235,186 @@ export class Dashboard extends Component {
   }
 
   render() {
+    const { stats } = this.props;
+
+    const campaignsPlot = {
+      labels: [],
+      datasets: [
+        {
+          backgroundColor: "transparent",
+          borderColor: "#fff",
+          data: stats.campaigns.plot
+        }
+      ]
+    };
+
+    const connectionsPlot = {
+      labels: [],
+      datasets: [
+        {
+          backgroundColor: "transparent",
+          borderColor: "#fff",
+          data: stats.connections.plot
+        }
+      ]
+    };
+
+    const customersPlot = {
+      labels: [],
+      datasets: [
+        {
+          backgroundColor: "rgba(255,255,255,.3)",
+          borderColor: "transparent",
+          data: stats.customers.plot
+        }
+      ]
+    };
+
+    const establishmentsPlot = {
+      labels: [],
+      datasets: [
+        {
+          backgroundColor: "transparent",
+          borderColor: "#fff",
+          data: [stats.establishments.plot]
+        }
+      ]
+    };
+
+    const cardChartOpts1 = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              color: "transparent",
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              fontSize: 2,
+              fontColor: "transparent"
+            }
+    
+          }
+        ],
+        yAxes: [
+          {
+            display: false,
+            ticks: {
+              display: false,
+              min: Math.min.apply(Math, stats.connections.plot),
+              max: Math.max.apply(Math, stats.connections.plot)
+            }
+          }
+        ]
+      },
+      elements: {
+        line: {
+          borderWidth: 1
+        },
+        point: {
+          radius: 4,
+          hitRadius: 10,
+          hoverRadius: 4
+        }
+      }
+    };
+    
+    const cardChartOpts2 = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              color: "transparent",
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              fontSize: 2,
+              fontColor: "transparent"
+            }
+    
+          }
+        ],
+        yAxes: [
+          {
+            display: false,
+            ticks: {
+              display: false,
+              min: Math.min.apply(Math, stats.establishments.plot),
+              max: Math.max.apply(Math, stats.establishments.plot)
+            }
+          }
+        ]
+      },
+      elements: {
+        line: {
+          tension: 0.00001,
+          borderWidth: 1
+        },
+        point: {
+          radius: 4,
+          hitRadius: 10,
+          hoverRadius: 4
+        }
+      }
+    };
+    
+    const cardChartOpts3 = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            display: false
+          }
+        ],
+        yAxes: [
+          {
+            display: false
+          }
+        ]
+      },
+      elements: {
+        line: {
+          borderWidth: 2
+        },
+        point: {
+          radius: 0,
+          hitRadius: 10,
+          hoverRadius: 4
+        }
+      }
+    };
+    
+    const cardChartOpts4 = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [
+          {
+            display: false,
+            barPercentage: 0.6
+          }
+        ],
+        yAxes: [
+          {
+            display: false
+          }
+        ]
+      }
+    };
+
     const tableArray = [
       1,
       2,
@@ -750,32 +431,34 @@ export class Dashboard extends Component {
           Westside Shopping Center</label>
       </td>
     </tr>);
+
+    console.log(cardChartOpts2);
     return (<div className="animated fadeIn" style={{
         marginTop: 20
       }}>
       <Row>
         <Col xs="12" lg="6">
-          <DashboardPanel color="#F15A24" chartData={cardChartData1} displayData={{
+          <DashboardPanel color="#F15A24" chartData={connectionsPlot} displayData={{
               title: "Partners",
-              value: "243"
+              value: stats.connections.count
             }} options={cardChartOpts1} type="line"/>
         </Col>
         <Col xs="12" lg="6">
-          <DashboardPanel color="#F7931E" chartData={cardChartData2} displayData={{
+          <DashboardPanel color="#F7931E" chartData={establishmentsPlot} displayData={{
               title: "Communication Diffusion",
-              value: "142000"
+              value: stats.establishments.count
             }} options={cardChartOpts2} type="line"/>
         </Col>
         <Col xs="12" lg="6">
-          <DashboardPanel color="#FBB03B" chartData={cardChartData3} displayData={{
+          <DashboardPanel color="#FBB03B" chartData={campaignsPlot} displayData={{
               title: "Campaigns",
-              value: 623
+              value: stats.campaigns.count
             }} options={cardChartOpts3} type="line"/>
         </Col>
         <Col xs="12" lg="6">
-          <DashboardPanel color="#F9DA23" chartData={cardChartData4} displayData={{
+          <DashboardPanel color="#F9DA23" chartData={customersPlot} displayData={{
               title: "Clients",
-              value: 356
+              value: stats.customers.count
             }} options={cardChartOpts4} type="bar"/>
         </Col>
       </Row>
