573659d5415173b743996b5aa9a483bc3668b46f
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-06-11 21:20:04 +0200

    Force HTTPS in production.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 import {HashRouter, Route, Switch, Redirect} from "react-router-dom";
 import {Provider} from "react-redux";
+import HttpsRedirect from 'react-https-redirect';
 
 import store from "./store";
 
@@ -29,34 +30,39 @@ import ForgotPassword from "./views/Pages/ForgotPassword/";
 import ResetPassword from "./views/Pages/ResetPassword/ResetPassword";
 import Page404 from "./views/Pages/Page404/";
 import Page500 from "./views/Pages/Page500/";
-import { getJwt } from "./services/CookieService";
+import {getJwt} from "./services/CookieService";
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(props) => (
-        getJwt()
-            ? <Component {...props} />
-            : <Redirect to={{
-                pathname: "/login",
-                state: { from: props.location }
-            }} />
-    )} />
-);
+const PrivateRoute = ({
+  component: Component,
+  ...rest
+}) => (<Route {...rest} render={(props) => (
+    getJwt()
+    ? <Component {...props}/>
+    : <Redirect to={{
+        pathname: "/login",
+        state: {
+          from: props.location
+        }
+      }}/>)}/>);
 PrivateRoute.propTypes = {
   component: PropTypes.func,
   location: PropTypes.object
 };
 
 ReactDOM.render((<Provider store={store}>
-  <HashRouter>
-    <Switch>
-      <Route exact path="/login" name="Login Page" component={Login}/>
-      <Route exact path="/logout" name="Logout Page" component={Logout}/>
-      <Route exact path="/register" name="Register Page" component={Register}/>
-      <Route exact path="/forgot-password" name="Forgot Password Page" component={ForgotPassword}/>
-      <Route exact path="/reset-password/:uid/:token" name="Reset Password Page" component={ResetPassword}/>
-      <Route exact path="/404" name="Page 404" component={Page404}/>
-      <Route exact path="/500" name="Page 500" component={Page500}/>
-      <PrivateRoute path="/" name="Home" component={Full}/>
-    </Switch>
-  </HashRouter>
-</Provider>), document.getElementById("root"));
+  <HttpsRedirect>
+    <HashRouter>
+      <Switch>
+        <Route exact="exact" path="/login" name="Login Page" component={Login}/>
+        <Route exact="exact" path="/logout" name="Logout Page" component={Logout}/>
+        <Route exact="exact" path="/register" name="Register Page" component={Register}/>
+        <Route exact="exact" path="/forgot-password" name="Forgot Password Page" component={ForgotPassword}/>
+        <Route exact="exact" path="/reset-password/:uid/:token" name="Reset Password Page" component={ResetPassword}/>
+        <Route exact="exact" path="/404" name="Page 404" component={Page404}/>
+        <Route exact="exact" path="/500" name="Page 500" component={Page500}/>
+        <PrivateRoute path="/" name="Home" component={Full}/>
+      </Switch>
+    </HashRouter>
+  </HttpsRedirect>
+
+        </Provider>), document.getElementById("root"));
