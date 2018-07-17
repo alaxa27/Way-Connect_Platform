f46423d7cbea4e5fd1a4f58e3abde52632204a4c
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-17 19:28:14 +0200

    lint fix for index

--- src/index.js
+++ src/index.js
@@ -51,18 +51,18 @@ PrivateRoute.propTypes = {
 };
 
 ReactDOM.render((<Provider store={store}>
-    <HashRouter>
-      <Switch>
-        <Route exact={true} path="/login" name="Login Page" component={Login}/>
-        <Route exact={true} path="/logout" name="Logout Page" component={Logout}/>
-        <Route exact={true} path="/register" name="Register Page" component={Register}/>
-        <Route exact={true} path="/register/confirm" name="Confirmation Sent Page" component={ConfirmationSent}/>
-        <Route exact={true} path="/confirm-registration/:activateUrl" name="Confirm Registration Page" component={ConfirmRegistration}/>
-        <Route exact={true} path="/forgot-password" name="Forgot Password Page" component={ForgotPassword}/>
-        <Route exact={true} path="/reset-password/:uid/:token" name="Reset Password Page" component={ResetPassword}/>
-        <Route exact={true} path="/404" name="Page 404" component={Page404}/>
-        <Route exact={true} path="/500" name="Page 500" component={Page500}/>
-        <PrivateRoute path="/" name="Home" component={Full}/>
-      </Switch>
-    </HashRouter>
+  <HashRouter>
+    <Switch>
+      <Route exact={true} path="/login" name="Login Page" component={Login}/>
+      <Route exact={true} path="/logout" name="Logout Page" component={Logout}/>
+      <Route exact={true} path="/register" name="Register Page" component={Register}/>
+      <Route exact={true} path="/register/confirm" name="Confirmation Sent Page" component={ConfirmationSent}/>
+      <Route exact={true} path="/confirm-registration/:activateUrl" name="Confirm Registration Page" component={ConfirmRegistration}/>
+      <Route exact={true} path="/forgot-password" name="Forgot Password Page" component={ForgotPassword}/>
+      <Route exact={true} path="/reset-password/:uid/:token" name="Reset Password Page" component={ResetPassword}/>
+      <Route exact={true} path="/404" name="Page 404" component={Page404}/>
+      <Route exact={true} path="/500" name="Page 500" component={Page500}/>
+      <PrivateRoute path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
 </Provider>), document.getElementById("root"));
