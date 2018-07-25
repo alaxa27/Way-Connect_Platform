fc137b8908584b0f3f478500912fb73d1d1573c4
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-25 12:17:43 +0200

    mock react-i18next.t() to pass tests.

--- tests/SetupTests.js
+++ tests/SetupTests.js
@@ -2,7 +2,13 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
+jest.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate HoC receive the t function as a prop
+  translate: () => Component => {
+    Component.defaultProps = { ...Component.defaultProps, t: () => "" };
+    return Component;
+  },
+}));
 /* Mock storages */
 (function () {
 
