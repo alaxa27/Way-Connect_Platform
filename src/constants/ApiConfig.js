d602eeb2b7d48c5915b9a43ed97447ed58f39a7a
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-07-31 18:49:10 +0200

    API host is staging in dev. lint

--- src/constants/ApiConfig.js
+++ src/constants/ApiConfig.js
@@ -1,9 +1,29 @@
 import axios from "axios";
+import { each, map, isNumber, isArray, isObject } from "underscore";
 
 let backendHost;
 const apiVersion = "v1";
 
 const hostname = window && window.location && window.location.hostname;
+
+const formatResponse = (data) => {
+  each(data, (value, key) => {
+    if(isNumber(value)) {
+        data[key] = floor(value);
+    } else if(isArray(value)) {
+      if(key !== "coordinates") {
+        const newArray = map(value, item => floor(item));
+        data[key] = newArray;
+      }
+    } else if(isObject(value)) {
+      formatResponse(value);
+    }
+  });
+};
+
+const floor = (value) => {
+  return Math.floor(value * 100) / 100;
+};
 
 if (hostname === "way-connect.com") {
   backendHost = "https://api.way-connect.com";
@@ -12,7 +32,16 @@ if (hostname === "way-connect.com") {
 } else {
   backendHost = "http://localhost:8000";
 }
-backendHost = "https://api.way-connect.com";
+
+if (process.env.NODE_ENV === "production") {
+  backendHost = "https://api.way-connect.com";
+} else if(process.env.NODE_ENV === "development") {
+  backendHost = "https://wayconnect-staging.herokuapp.com";
+}
 export const axiosInstance = axios.create({
   baseURL: backendHost,
+  transformResponse: axios.defaults.transformResponse.concat((data) => {
+    formatResponse(data);
+    return data;
+  })
 });
