30e28afc7165634b2dd6332b599446fe3a1a75e0
volodymyrhudyma <volodymyrhudyma@gmail.com>
2018-08-16 21:40:34 +0200

    Intergrated wallet manager

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,22 +1,67 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import {translate} from "react-i18next";
+import { Progress } from 'reactstrap';
+import { map } from 'underscore';
 
 class Aside extends Component {
   render() {
     const { t } = this.props;
+    const history = [0,1,2,3,4,5,6,7,8,9];
     return (
-      <aside className="aside-menu text-center px-2">
-        <div className="aside-menu__title aside-menu__title--colored mt-4">
-          {t("walletManager.top")}
-        </div>
-        <div className="aside-menu__coming-soon">
-          <img src="../img/shiba-01.png" alt="Logo" className="modal-body__img" />
-          <div className="aside-menu__title aside-menu__title--big mb-3">
+      <aside className="aside-menu">
+        <div className="aside-menu__top my-4 pb-4 mx-3">
+          <div className="aside-menu__title">
             {t("walletManager.title")}
           </div>
-          <div className="aside-menu__title">
-            {t("walletManager.subtitle")}
+          <div className="currency-icon">
+            +<i className="fa fa-usd"></i>
+          </div>
+        </div>
+        <div className="aside-menu__wallet">
+          <div className="aside-menu__budget px-3 mb-4">
+            <div className="aside-menu__budget-item" style={{flexGrow: "0.5", background: "orange"}}>
+              <div className="aside-menu__budget-label ml-4">
+                <div className="aside-menu__budget-label--small">
+                  Budget total
+                </div>
+                <div className="aside-menu__budget-label--big">
+                  4000 WC
+                </div>
+              </div>
+            </div>
+            <div className="aside-menu__budget-item" style={{flexGrow: "0.5", background: "grey"}}>
+              <div className="aside-menu__budget-label ml-4">
+                <div className="aside-menu__budget-label--small">
+                  Budget total
+                </div>
+                <div className="aside-menu__budget-label--big">
+                  4000 WC
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="aside-menu__transactions p-3">
+            <div className="aside-menu__transactions-title mb-2">
+              Transaction history
+            </div>
+            {map(history, item => {
+              return (
+                <div className="aside-menu__transactions-item px-3 py-1 mb-2">
+                  <div className="aside-menu__transactions-box--small">
+                    <div>
+                      Cash
+                    </div>
+                    <div>
+                      25/08/2018 18:52
+                    </div>
+                  </div>
+                  <div className="aside-menu__transactions-box aside-menu__transactions-box--big">
+                    550 WC
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </aside>
--- src/views/Campaigns/BidCampaign/BidTotal.js
+++ src/views/Campaigns/BidCampaign/BidTotal.js
@@ -8,7 +8,7 @@ class BidTotal extends Component {
     return (
       <div className="mb-4 bid-total">
         <InputRange maxValue={data.max} minValue={data.min} value={data.value} disabled={true} onChange={(val) => {console.log(val);}} />
-        <div className="bid-total__currency">
+        <div className="currency-icon">
           +<i className="fa fa-usd"></i>
         </div>
       </div>
