4f3cddcbdee18bc6647f14f794bdb06c51d4c404
Taqiyeddine SAKMECHE <alaxa27@gmail.com>
2018-09-30 21:18:05 +0200

    Env config setup.

--- webpack.config.js
+++ webpack.config.js
@@ -118,7 +118,7 @@ module.exports = (env = {}) => {
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: JSON.stringify(env.NODE_ENV),
-          STAGE: JSON.stringify(env.STAGE),
+          STAGE: JSON.stringify(process.env.STAGE),
         }
       })
     ]
