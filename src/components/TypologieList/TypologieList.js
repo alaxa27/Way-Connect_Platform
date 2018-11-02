42f493bc0b5c8d6a164dbdac837433c01ef690c9
volodymyrhudyma <volodymyrhudyma@gmail.com>
2018-11-02 17:07:20 +0100

    Handled optional product desc field / video upload on submit button click

--- src/components/TypologieList/TypologieList.js
+++ src/components/TypologieList/TypologieList.js
@@ -60,9 +60,6 @@ class TypologieList extends Component {
             <div className="input-wrapper">
               <label>Product description</label>
               <Input type="text" id="productDescription" className="typologie-list__input" name="productDescription" value={productDescription} onChange={(e) => updateCampaignProperty({ name: "productDescription", value: e.target.value}) }/>
-              {validator.message("productDescription", productDescription, "required", "text-danger", {
-                required: this.errorMessageService.generateErrorMessage("Product description", "required"),
-              })}
             </div>
           </Col>
         </Row>
